3b1a6fcbf29aa2f69e56223d01908943bba25aa2
Paul BERNARD <pbernard007@ensc.fr>
2025-12-04 09:58:09 +0100

    Allow calling point generation from terminal

--- generate.py
+++ generate.py
@@ -1,8 +1,11 @@
 import math
+import sys
 
 def generate(n,size):
     r = size*0.9/2
     tableau = []
     for i in range(n):
-        tableau.append([round(size/2+r*math.cos(i*2*math.pi/n)),round(size/2+r*math.sin(i*2*math.pi/n))])
+        tableau.append([round(size/2-r*math.cos(i*2*math.pi/n)),round(size/2+r*math.sin(i*2*math.pi/n))])
     return tableau
+
+print(generate(int(sys.argv[1]),int(sys.argv[2])))
